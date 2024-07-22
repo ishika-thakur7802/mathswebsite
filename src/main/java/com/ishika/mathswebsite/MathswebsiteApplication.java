3bd6bf1d2984d0f9018be6464054bdb5ceca122f
Ishika Thakur <ishikathakur0708@gmail.com>
2024-07-23 02:16:16 +0530

    Initialised spring project

---
+++ src/main/java/com/ishika/mathswebsite/MathswebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.ishika.mathswebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathswebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathswebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ishika/mathswebsite/MathswebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishika.mathswebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathswebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
